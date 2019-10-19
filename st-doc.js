38cf80ed6cde0d34ac603f72002e873aa7146edb
zoolabzinid <55064896+zoolabzinid@users.noreply.github.com>
2019-10-19 13:52:31 +0200

    Create st-doc.js

---
+++ st-doc.js
@@ -0,0 +1,476 @@
+if(window.location.pathname == "/st/WhatsApp-Plus-v7-35-apk")
+{
+document.write('WhatsApp-Plus v7.35 apk')
+}
+if(window.location.pathname == "/st/WhatsApp-Plus-v7-25-apk")
+{
+document.write('WhatsApp-Plus v7.25 apk')
+}
+if(window.location.pathname == "/st/GBWhatsApp-v6-89-apk")
+{
+document.write('GBWhatsApp v6.89 apk')
+}
+if(window.location.pathname == "/st/GBWhatsApp-v7-25_Anti-Ban-apk")
+{
+document.write('GBWhatsApp Anti-Ban -v7-25 apk')
+}
+if(window.location.pathname == "/st/GBWhatsApp-v7-35_Anti-Ban-apk")
+{
+document.write('GBWhatsApp Anti-Ban v7.35 apk')
+}
+if(window.location.pathname == "/st/GBWhatsApp-v7-20_Anti-Ban-apk")
+{
+document.write('GBWhatsApp Anti-Ban v7.20 apk')
+}
+if(window.location.pathname == "/st/Gbwhatsapp-v7-00-apk")
+{
+document.write('Gbwhatsapp v7.00 apk')
+}
+if(window.location.pathname == "/st/PUBG-Momile-v0-13-0-apk")
+{
+document.write('PUBG-Momile v0.13.0 apk')
+}
+if(window.location.pathname == "/st/PUBG-Momile-v0-3-0-main-obb-zip")
+{
+document.write('PUBG-Momile v0.3.0 main obb zip')
+}
+if(window.location.pathname == "/st/PUBG-Momile-v0-13-0-main-obb-zip")
+{
+document.write('PUBG-Momile v0.13.0 main obb zip')
+}
+if(window.location.pathname == "/st/http-injecotr-lite-apk")
+{
+document.write('http-injecotr lite apk latest')
+}
+if(window.location.pathname == "/st/ehil-config-file1")
+{
+document.write('ehi lite config file1')
+}
+if(window.location.pathname == "/st/ehil-config-file2")
+{
+document.write('ehi lite config file2')
+}
+if(window.location.pathname == "/st/lucky-Patcher-Official-8-4-1-apk")
+{
+document.write('lucky-Patcher-Official v8.4.1 apk')
+}
+if(window.location.pathname == "/st/lucky-patcher-mod-v8-3-8-zip")
+{
+document.write('lucky-patcher mod v8.3.8 zip')
+}
+if(window.location.pathname == "/st/pes-2019-V3-2-0-apk")
+{
+document.write('pes 2019 V3.2.0 apk')
+}
+if(window.location.pathname == "/st/pes-2019-3-2-0-main-obb-zip")
+{
+document.write('pes 2019 v3.2.0 main obb zip file')
+}
+if(window.location.pathname == "/st/pes-2019-v3-2-0-patch-obb-zip")
+{
+document.write('pes 2019 v3.2.0 patch obb zip file')
+}
+if(window.location.pathname == "/st/pes-2019-3-2-0-main-obb")
+{
+document.write('Pes 2019 v3.2.0 main obb')
+}
+if(window.location.pathname == "/st/PES-2019-v3-2-0-patch-obb")
+{
+document.write('PES 2019 v3.2.0 patch obb')
+}
+if(window.location.pathname == "/st/Odin3-v3-11-1-Downloader")
+{
+document.write('Odin3 Downloader v3.11.1')
+}
+if(window.location.pathname == "/st/recovery-tar-md5")
+{
+document.write('recovery tar md5')
+}
+if(window.location.pathname == "/st/SuperSU-v2-79-zip")
+{
+document.write('SuperSU v2.79 zip')
+}
+if(window.location.pathname == "/st/IDM-plus-v9-5")
+{
+document.write('IDM-plus v9.5 | up-4ver')
+}
+if(window.location.pathname == "/st/IDM-mod")
+{
+document.write('IDM mod apk')
+}
+if(window.location.pathname == "/st/Asphalt8-Airborne-v4-0-2a")
+{
+document.write('Asphalt8-Airborne v4.0.2a')
+}
+if(window.location.pathname == "/st/Asphalt8-Airborne-mod-apk")
+{
+document.write('Asphalt8-Airborne mod apk')
+}
+if(window.location.pathname == "/st/textnow")
+{
+document.write('textnow free us phone number')
+}
+if(window.location.pathname == "/st/dl-talkatone-v6-3-10-apk")
+{
+document.write('talkatone v6-3.10 apk')
+}
+if(window.location.pathname == "/st/talkatone-v6-3-10-apk")
+{
+document.write('talkatone v6.3.10 apk')
+}
+if(window.location.pathname == "/st/TextPlus")
+{
+document.write('TextPlus free phone number')
+}
+if(window.location.pathname == "/st/DingTone-apk")
+{
+document.write('DingTone apk')
+}
+if(window.location.pathname == "/st/mirror-DingTone-apk")
+{
+document.write('mirror DingTone apk')
+}
+if(window.location.pathname == "/st/latest-http-injector")
+{
+document.write('latest http injector')
+}
+if(window.location.pathname == "/st/sedc-ehi-config-usa")
+{
+document.write('sedc ehi config usa')
+}
+if(window.location.pathname == "/st/sedc-ehi-config-germany")
+{
+document.write('sedc ehi config De')
+}
+if(window.location.pathname == "/st/sedc-ehi-singapore")
+{
+document.write('sedc ehi singapore')
+}
+if(window.location.pathname == "/st/sparkvpn")
+{
+document.write('Latest sparkvpn')
+}
+if(window.location.pathname == "/st/sedc-spark-config")
+{
+document.write('sedc spark config')
+}
+if(window.location.pathname == "/st/sedc-spark-config-germany")
+{
+document.write('sedc spark config de')
+}
+if(window.location.pathname == "/st/sedc-spark-config-singapore")
+{
+document.write('sedc spark config singapore')
+}
+if(window.location.pathname == "/st/snaptube-vip-apk")
+{
+document.write('snaptube vip apk')
+}
+if(window.location.pathname == "/st/PUBG_MOBILE_LITE_v0-14-0-apk")
+{
+document.write('PUBG MOBILE LITE v0.14.0 apk')
+}
+if(window.location.pathname == "/st/PUBG_MOBILE_LITE_0-14-0-obb")
+{
+document.write('PUBG_MOBILE LITE v0.14.0 obb.zip')
+}
+if(window.location.pathname == "/st/PUBG-MOBILE-LITE-v0-12-0-apk")
+{
+document.write('PUBG-MOBILE LITE v0.12.0 apk')
+}
+if(window.location.pathname == "/st/PUBG-MOBILE-LITE-v0-12-0-main-obb-zip")
+{
+document.write('PUBG-MOBILE LITE v0.12.0 main obb zip')
+}
+if(window.location.pathname == "/st/Mercs-of-Boom-v2-0-20-apk")
+{
+document.write('Mercs of Boom v2.0.20 apk')
+}
+if(window.location.pathname == "/st/mirror-Mercs-of-Boom")
+{
+document.write('Mercsof Boom')
+}
+if(window.location.pathname == "/st/ktr-opener")
+{
+document.write('ktr opener model ap')
+}
+if(window.location.pathname == "/st/kpn-tunnel-revolution")
+{
+document.write('kpn tunnel revolution ')
+}
+if(window.location.pathname == "/st/kpn-tunnel")
+{
+document.write('kpn tunnel vpn')
+}
+if(window.location.pathname == "/st/kpn-tunnel-ultimate")
+{
+document.write('kpn tunnel ultimate')
+}
+if(window.location.pathname == "/st/xposed")
+{
+document.write('latest xposed installer')
+}
+if(window.location.pathname == "/st/kingroot")
+{
+document.write('kingroot apk')
+}
+if(window.location.pathname == "/st/rootchecker")
+{
+document.write('rootchecker apk')
+}
+if(window.location.pathname == "/st/setdns")
+{
+document.write('setdns apk')
+}
+if(window.location.pathname == "/st/overridedns")
+{
+document.write('overridedns apk')
+}
+if(window.location.pathname == "/st/ehi-config-canada")
+{
+document.write('ehi config CA')
+}
+if(window.location.pathname == "/st/ehi-config-uk")
+{
+document.write('ehi config uk')
+}
+if(window.location.pathname == "/st/ehi-config-usa")
+{
+document.write('ehi config usa')
+}
+if(window.location.pathname == "/st/kpn-config-usa")
+{
+document.write('kpn config usa')
+}
+if(window.location.pathname == "/st/spark-config-mtn-1")
+{
+document.write('spark config mtn-1')
+}
+if(window.location.pathname == "/st/spark-config=mtn-2")
+{
+document.write('spark config mtn-2')
+}
+if(window.location.pathname == "/st/spark-config=mtn-3")
+{
+document.write('spark config mtn-3')
+}
+if(window.location.pathname == "/st/X-plorer")
+{
+document.write('X-plorer file manager')
+}
+if(window.location.pathname == "/st/es-xplorer")
+{
+document.write('ES Xplorer File manager apk')
+}
+if(window.location.pathname == "/st/http-proxy-injector-pc")
+{
+document.write('http proxy injector for PC')
+}
+if(window.location.pathname == "/st/xposed-installer")
+{
+document.write('xposed installer')
+}
+if(window.location.pathname == "/st/kpn-opener-v0-3-apk")
+{
+document.write('kpn opener v0.3 apk')
+}
+if(window.location.pathname == "/st/mobogram")
+{
+document.write('mobogram Alternatvie to official')
+}
+if(window.location.pathname == "/st/epro-opener-v0-3-6")
+{
+document.write('epro opener v0.3.6')
+}
+if(window.location.pathname == "/st/eproxy-nightly-v-2-7-29")
+{
+document.write('eproxy Nightly v2.7.29')
+}
+if(window.location.pathname == "/st/eproxy-v2-9-34")
+{
+document.write('Latest eproxy v2.9.34')
+}
+if(window.location.pathname == "/st/mobogram-one")
+{
+document.write('mobogram one apk')
+}
+if(window.location.pathname == "/st/mobogram-two")
+{
+document.write('mobogram two apk')
+}
+if(window.location.pathname == "/st/mobogram-three")
+{
+document.write('mobogram three')
+}
+if(window.location.pathname == "/st/ehiopener48")
+{
+document.write('ehiopener48& apk')
+}
+if(window.location.pathname == "/st/injectotr341")
+{
+document.write('injectotr341 apk')
+}
+if(window.location.pathname == "/st/ehiopener47")
+{
+document.write('ehiopener47')
+}
+if(window.location.pathname == "/st/injector430")
+{
+document.write('httpinjector430')
+}
+if(window.location.pathname == "/st/EaseUS-MobiSaver-apk")
+{
+document.write('EaseUS MobiSaver apk')
+}
+if(window.location.pathname == "/st/netify-vpn")
+{
+document.write('netify vpn')
+}
+if(window.location.pathname == "/st/psyphon-pro-lite-handler")
+{
+document.write('psyphon Pro lite handler')
+}
+if(window.location.pathname == "/st/Psiphon82HandlerUI-apk")
+{
+document.write('Psiphon82HandlerUI-apk')
+}
+if(window.location.pathname == "/st/Ultrasurf-Handler-vpn")
+{
+document.write('Ultrasurf-Handler-vpn')
+}
+if(window.location.pathname == "/st/injector-43073-apk")
+{
+document.write('injector-43073-apk')
+}
+if(window.location.pathname == "/st/ehiopener46")
+{
+document.write('ehiopener46')
+}
+if(window.location.pathname == "/st/injector42371")
+{
+document.write('HTTPinjector42371')
+}
+if(window.location.pathname == "/st/ehiopener-unlocker")
+{
+document.write('ehiopener unlocker')
+}
+if(window.location.pathname == "/st/luckypatcher-apk")
+{
+document.write('lucky patcher apk')
+}
+if(window.location.pathname == "/st/grand-theft-v1-07-apk")
+{
+document.write('grand theft v1.07 apk')
+}
+if(window.location.pathname == "/st/grand-theft-v1-07-zip")
+{
+document.write('grand theft v1.07 zip')
+}
+if(window.location.pathname == "/st/Samsung-USB-Drivers")
+{
+document.write('Samsung USB Drivers')
+}
+if(window.location.pathname == "/st/TWRP-S7582-tar-md5")
+{
+document.write('TWRP S7582 tar md5')
+}
+if(window.location.pathname == "/st/WhatsApp-Plus-v8-00-apk")
+{
+document.write('WhatsApp Plus v8.00 apk')
+}
+if(window.location.pathname == "/st/GBWhatsApp-Pro-v8-00-apk")
+{
+document.write('GBWhatsApp Pro v8.00 apk')
+}
+if(window.location.pathname == "/st/dr-whatsapp-plus-8-00-apk")
+{
+document.write('dr-whatsapp plus v8.00 apk')
+}
+if(window.location.pathname == "/st/PUBG-Mobile-0-14-0-apk")
+{
+document.write('PUBG Mobile v0.14.0 apk')
+}
+if(window.location.pathname == "/st/PUBG.Mobile-v0-14-0-main-obb-zip")
+{
+document.write('PUBG Mobile v0.14.0 main obb zip')
+}
+if(window.location.pathname == "/st/dr-PUBG-Momile-v0-14-0-apk")
+{
+document.write('PUBG Momile v0.14.0 apk')
+}
+if(window.location.pathname == "/st/dr-PUBG-Momile-v0-14-0-main-obb-zip")
+{
+document.write('PUBG Momile v0.14.0 main obb zip')
+}
+if(window.location.pathname == "/st/Lolipop-Extreme-zip")
+{
+document.write('Lolipop Extreme zip')
+}
+if(window.location.pathname == "/st/preactivated-zip")
+{
+document.write('Preactivated zip')
+}
+if(window.location.pathname == "/st/dls-19-ucl-apk")
+{
+document.write('DLS 19 UCL apk')
+}
+if(window.location.pathname == "/st/dsl-2019-obb-data-ucl-zip")
+{
+document.write('DLS 2019 OBB DATA UCL zip')
+}
+if(window.location.pathname == "/st/dls-2019-ucl-data-file-zip")
+{
+document.write('DLS 2019 UCL Data file zip')
+}
+if(window.location.pathname == "/st/Dream_League_Soccer_2018_v5-064_apk")
+{
+document.write('Dream League Soccer 2018 v5.064 apk')
+}
+if(window.location.pathname == "/st/Dream-League-main-82-obb")
+{
+document.write('Dream League main 82 obb')
+}
+if(window.location.pathname == "/st/Dream_League_Soccer_2018_v5-053_apk")
+{
+document.write('Dream League Soccer 2018 v5.053 apk')
+}
+if(window.location.pathname == "/st/Dream_league_main_59_obb_zip")
+{
+document.write('Dream league main 59 obb zip')
+}
+if(window.location.pathname == "/st/dream_league_v6-13-109-apk")
+{
+document.write('Dream league 109 v6.13 apk')
+}
+if(window.location.pathname == "/st/dream_league_2019_109_obb_zip")
+{
+document.write('Dream league 109 2019 obb zip')
+}
+if(window.location.pathname == "/st/dl/dream-league-2019-6-11-apk")
+{
+document.write('Dream league 2019 V6.11 apk')
+}
+if(window.location.pathname == "/st/dream-league-2019-main-104-obb-zip")
+{
+document.write('Dream league 2019 main obb 104 zip')
+}
+if(window.location.pathname == "/st/dream-league-2019-6-7-apk")
+{
+document.write('Dream league 2019 V6.7 apk')
+}
+if(window.location.pathname == "/st/dream-league-main-98-obb-zip")
+{
+document.write('Dream league  98 main obb zip')
+}
+if(window.location.pathname == "/st/PUBG_MOBILE_v0-15-0-apk")
+{
+document.write('PUBG MOBILE v0.15.0 apk')
+}
+if(window.location.pathname == "/st/PUBG-MOBILE-V0-15-0-obb-zip")
+{
+document.write('PUBG MOBILE V0.15.0 obb data file')
+}
+if(window.location.pathname == "/st/Graphic-tool-for-Battleground-v4-5-1-apk")
+{
+document.write('Graphic tool for- Battleground v4.5.1 apk')
+}
