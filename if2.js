c516de1ef7c1e39f1c1d675df6b82ffe436e88d6
zoolabzinid <55064896+zoolabzinid@users.noreply.github.com>
2019-10-21 10:12:57 +0200

    Create if2.js

---
+++ if2.js
@@ -0,0 +1,1 @@
+document.writeln('<iframe src="https://onedrive.live.com/embed?cid=0EBD853649D6ACE3&resid=EBD853649D6ACE3%21109&authkey=AHEemrIJzy7y_pk" width="98" height="120" frameborder="0" scrolling="no"></iframe>');
